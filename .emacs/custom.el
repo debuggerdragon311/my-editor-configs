229e400eb01d800ab143dec99ddfda14d91f705b
debuggerdragon311 <ayushkantibala2020.com>
2026-02-28 08:21:41 -0800

    Add Emacs configuration files

---
+++ .emacs/custom.el
@@ -0,0 +1,18 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("fffef514346b2a43900e1c7ea2bc7d84cbdd4aa66c1b51946aade4b8d343b55a"
+     "7771c8496c10162220af0ca7b7e61459cb42d18c35ce272a63461c0fc1336015" default))
+ '(package-selected-packages '(@))
+ '(warning-suppress-log-types '((treesit))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'customize-themes 'disabled nil)
